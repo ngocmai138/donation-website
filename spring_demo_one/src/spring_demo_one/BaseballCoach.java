84a5177fe1ea3fdb59361ba7707978f8a569c0af
ngocmai138 <nnm0891@gmail.com>
2023-11-08 16:44:28 +0700

    60

---
+++ spring_demo_one/src/spring_demo_one/BaseballCoach.java
@@ -0,0 +1,7 @@
+package spring_demo_one;
+
+public class BaseballCoach implements Coach{
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+}
---
+++ spring_demo_one/src/spring_demo_one/MyApp.java
@@ -0,0 +1,10 @@
+package spring_demo_one;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		Coach theCoach = new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
