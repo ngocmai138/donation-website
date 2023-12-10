d3a2cc798c719ec4ef78bb9f51f3617ce6d22988
ngocmai138 <nnm0891@gmail.com>
2023-12-10 16:18:00 +0700

    98

--- Lab4/src/main/java/com/example/springdemo/mvc/CustomerController.java
+++ Lab4/src/main/java/com/example/springdemo/mvc/CustomerController.java
@@ -5,12 +5,10 @@ import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@Validated
 @RequestMapping("/customer")
 public class CustomerController {
 	
